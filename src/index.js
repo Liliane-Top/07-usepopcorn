b88f6a2fbebb1697cc378c551b6b7205cb826914
Liliane Top <hallo@lilianetop.nl>
2024-01-25 17:12:13 +0100

    improving reusalibility of component by giving access to state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
-    <StarRating maxRating={3} />
-    <StarRating />
+    <StarRating defaultRating={3} size={82} color={"red"} />
+    <StarRating
+      size={60}
+      messages={["bad", "mediocre", "okay", "good", "amazing"]}
+      color={"orange"}
+    />
+    <Test />
   </React.StrictMode>
 );
